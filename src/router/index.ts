b8bcc1d7c5e73748a8baf0c16d307061fd929d4e
rzx100 <770304867@qq.com>
2022-04-29 21:02:16 +0800

    拖拉拽

---
+++ src/router/index.ts
@@ -0,0 +1,35 @@
+import {
+  createRouter,
+  createWebHashHistory,
+  RouteRecordRaw,
+  RouteLocationNormalized,
+} from 'vue-router'
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/dg',
+    name: 'dg',
+    component: () => import('../pages/draggable/index.vue'),
+    meta: {
+      title: '登录',
+    },
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    //  处理路由切换滚动行为
+    return new Promise((resolve) => {
+      if (savedPosition) {
+        return savedPosition
+      }
+      if (from.meta.saveSrollTop) {
+        const top: number = document.documentElement.scrollTop || document.body.scrollTop
+        resolve({ left: 0, top })
+      }
+    })
+  },
+})
+export default router
